e80b50add7c6d37a29f96c718be7ceca9aa4e711
ArslanDev <arslan.mustafin.dev@gmail.com>
2020-10-17 16:26:50 +0500

    feat: complete task 3

--- src/resources/task/task.router.js
+++ src/resources/task/task.router.js
@@ -1,42 +1,82 @@
 const router = require('express').Router();
 const taskService = require('./task.service');
+const morganTokens = require('../../common/morgan-tokens');
+const logger = require('../../common/logger');
 
-router.get('/:boardId/tasks', async (req, res) => {
+router.get('/:boardId/tasks', morganTokens.tasks, async (req, res) => {
   const { boardId } = req.params;
-  const tasks = await taskService.getAllByBoardId(boardId);
-  res.status(200).send(tasks);
-});
-
-router.get('/:boardId/tasks/:taskId', async (req, res) => {
-  const { boardId, taskId } = req.params;
-  const task = await taskService.getTaskById(boardId, taskId);
-  if (task) {
-    res.status(200).send(task);
-  } else {
-    res.status(404).send('not found');
+  try {
+    const tasks = await taskService.getAllByBoardId(boardId);
+    res.status(200).send(tasks);
+  } catch (error) {
+    logger.error(error.stack);
   }
 });
 
-router.post('/:boardId/tasks', async (req, res) => {
+router.get('/:boardId/tasks/:taskId', morganTokens.tasks, async (req, res) => {
+  const { boardId, taskId } = req.params;
+  try {
+    const task = await taskService.getTaskById(boardId, taskId);
+    if (task) {
+      res.status(200).send(task);
+    } else {
+      res.status(404).send('not found');
+      logger.log({
+        level: 'error',
+        message: `Method: ${req.method} - Status: 404 - Error: Task Not Found! - boardId: ${boardId} | taskId: ${taskId}, Url: ${req.originalUrl}\n`
+      });
+    }
+  } catch (error) {
+    logger.error(error.stack);
+  }
+});
+
+router.post('/:boardId/tasks', morganTokens.tasks, async (req, res) => {
   const data = req.body;
   const { boardId } = req.params;
-
-  const createTask = await taskService.createTask(data, boardId);
-  res.send(createTask);
+  try {
+    const createTask = await taskService.createTask(data, boardId);
+    res.status(200).send(createTask);
+  } catch (error) {
+    logger.error(error.stack);
+  }
 });
 
-router.put('/:boardId/tasks/:taskId', async (req, res) => {
+router.put('/:boardId/tasks/:taskId', morganTokens.tasks, async (req, res) => {
   const { boardId, taskId } = req.params;
   const data = req.body;
-  const updateTask = await taskService.updateTaskById(boardId, taskId, data);
-
-  res.send(updateTask);
+  try {
+    const updateTask = await taskService.updateTaskById(boardId, taskId, data);
+    if (updateTask) {
+      res.status(200).send(updateTask);
+    } else {
+      res.status(404).send('not found');
+      logger.log({
+        level: 'error',
+        message: `Method: ${req.method} - Status: 404 - Error: Task Not Found! - boardId: ${boardId} | taskId: ${taskId}, Url: ${req.originalUrl}\n`
+      });
+    }
+  } catch (error) {
+    logger.error(error.stack);
+  }
 });
 
-router.delete('/:boardId/tasks/:taskId', async (req, res) => {
-  const { boardId, taskId } = req.params;
-  await taskService.deleteTaskById(boardId, taskId);
-  res.status(204).send('The board has been deleted');
-});
+router.delete(
+  '/:boardId/tasks/:taskId',
+  morganTokens.tasks,
+  async (req, res) => {
+    const { boardId, taskId } = req.params;
+    try {
+      await taskService.deleteTaskById(boardId, taskId);
+      res.status(204).send('The task has been deleted');
+    } catch (error) {
+      res.status(404).send('Task not found');
+      logger.log({
+        level: 'error',
+        message: `Method: ${req.method} - Status: 404 - Error: Task Not Found! - boardId: ${boardId} | taskId: ${taskId}, Url: ${req.originalUrl}\n`
+      });
+    }
+  }
+);
 
 module.exports = router;
